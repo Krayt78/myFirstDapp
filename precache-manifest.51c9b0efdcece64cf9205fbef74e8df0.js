513dac277fbbcd4d2f26277cba139873a1f48758
Krayt78 <ludovic.domingues96@gmail.com>
2022-04-20 18:28:27 +0200

    Updates

---
+++ precache-manifest.51c9b0efdcece64cf9205fbef74e8df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ee026fd8c9478b53c1f5fcb640bdc6",
+    "url": "/myFirstDapp/index.html"
+  },
+  {
+    "revision": "4d55910fd36cde47ffb7",
+    "url": "/myFirstDapp/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "9b9076a6c788563ca54d",
+    "url": "/myFirstDapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4d55910fd36cde47ffb7",
+    "url": "/myFirstDapp/static/js/2.c3e986f7.chunk.js"
+  },
+  {
+    "revision": "9b9076a6c788563ca54d",
+    "url": "/myFirstDapp/static/js/main.278430d6.chunk.js"
+  },
+  {
+    "revision": "589b0931073db863e6b1",
+    "url": "/myFirstDapp/static/js/runtime-main.80cd8a70.js"
+  }
+]);
